08d99460f9592fc6108e8acb433861159bfe6e83
Marys888 <severovamaha1999@gmail.com>
2025-05-21 14:30:47 +0200

    Add file handler implementation to create, write and read files

---
+++ src/app/FileHandler.java
@@ -0,0 +1,39 @@
+package app;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileHandler {
+
+    private static String createFile(String strPath){
+        Path newFile;
+        try{
+            newFile = Files.createFile(Path.of(strPath));
+        }catch(FileAlreadyExistsException e) {
+            return "File already exists!";
+        }catch(IOException e) {
+            return "Something wrong.";
+        }
+        return newFile + "created";
+    }
+
+    private static  String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "written into " + path;
+    }
+
+    private static String readFromFile(String path){
+
+        try {
+            return Files.readString(Path.of(path));
+        }catch(IOException e) {
+            return "something went wrong " + e.getMessage();
+        }
+    }
+}
+
