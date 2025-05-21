8c1b9ff30bdc0146885a240c3bf9312bff60b437
Marys888 <severovamaha1999@gmail.com>
2025-05-21 14:47:05 +0200

    Update application entry point to start the program

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,23 @@
 package app;
+import java.nio.file.Paths;
 
 public class Main {
+    private static final String BASE_PATH = "files/";
     public static void main(String[] args) {
-        
+        String fileFormat = ".txt";
+        String fileName = "myFile";
+        String path = BASE_PATH + fileName + fileFormat;
+        String content = "Super Information";
+
+        FileHandler handler = new FileHandler();
+
+        getOutput(handler.createFile(path));
+        getOutput(handler.writeToFile(Paths.get(path), content));
+        getOutput(handler.readFromFile(path));
+
+
+    }
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
